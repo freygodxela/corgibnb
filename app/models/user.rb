7824ebad648d0c2d4138955677a8718357fd986a
Alex Godfrey <alexgodfrey2@gmail.com>
2022-05-07 16:46:38 +0200

    seeds fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings
-  has_many :corgis
+  has_many :bookings, dependent: :destroy
+  has_many :corgis, dependent: :destroy
 end
